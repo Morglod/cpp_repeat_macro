03967425c69e0b40c9b578a4b052dd6f80b87803
Vladimir Kruzhkov <morglod@gmail.com>
2021-07-10 23:07:33 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 NAME="RPCT_REPEAT";
 MAX=128;
 
+echo "#define ${NAME}_MAX $MAX"
 echo "#define ${NAME}_1(EXPR) EXPR(1)"
 for i in {2..128}; do
     j=$((i-1));
@@ -10,6 +11,7 @@ done
 echo "#define $NAME(N, EXPR) ${NAME}_##N(EXPR)"
 
 # output:
+# #define RPCT_REPEAT_MAX 128
 # #define RPCT_REPEAT_1(EXPR) EXPR(1)
 # #define RPCT_REPEAT_2(EXPR) RPCT_REPEAT_1(EXPR) EXPR(2)
 # #define RPCT_REPEAT_3(EXPR) RPCT_REPEAT_2(EXPR) EXPR(3)
